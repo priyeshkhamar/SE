64a0929811e04e5a22f91139b9473ba7ea021481
priyeshkhamar <priyeshkhamar76@gmail.com>
2024-01-28 17:17:27 +0530

    28Jan

---
+++ Assignments/Assignment - 2 [Module - Fundamentals of Programming]/Basic Logic Programs/A13.c
@@ -0,0 +1,12 @@
+// Find character value from ascii
+
+#include<stdio.h>
+
+main()
+{
+    int ascii;
+    printf("Enter any ASCII Code : ");
+    scanf("%d",&ascii);
+
+    printf("The Character corresponding to %d ASCII Value is : %c ",ascii, ascii);
+}
---
+++ Assignments/Assignment - 2 [Module - Fundamentals of Programming]/Basic Logic Programs/A14.c
@@ -0,0 +1,12 @@
+// Find ascii value of given number
+
+#include<stdio.h>
+
+main()
+{
+    char c;
+    printf("Enter any Character/Number : ");
+    scanf("%c",&c);
+
+    printf("The ASCII Value corresponding to %c Character is : %d ",c, c);
+}
