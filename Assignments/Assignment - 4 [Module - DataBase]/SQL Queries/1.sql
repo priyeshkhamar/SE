230ad1b991c42d65e5b629ae68780e31367d1175
Priyesh Khamar <131170202+priyeshkhamar@users.noreply.github.com>
2024-03-28 12:10:33 +0530

    28Mar

---
+++ Assignments/Assignment - 4 [Module - DataBase]/SQL Queries/1.sql
@@ -0,0 +1,36 @@
+create database db1;
+
+use db1;
+
+create table students (
+Roll_No int auto_increment,
+Name varchar(50),
+Branch varchar(50),
+primary key (Roll_No)
+);
+
+CREATE TABLE Exam(
+Roll_No int,
+S_Code varchar(20),
+Marks int,
+P_Code varchar(20),
+FOREIGN KEY (Roll_No) REFERENCES students(Roll_No)
+);
+
+insert into students (Name, Branch) values
+("Jay","Computer Science"),
+("Suhani", "Electronics and Comm."),
+("Kriti", "Electronics and Comm");
+
+
+insert into exam(Roll_No, S_Code, Marks, P_Code)
+values 
+('1', 'CS11', '50', 'CS'),
+('1', 'CS12', '60', 'CS'),
+('2', 'EC101', '66', 'EC'),
+('2', 'EC102', '70', 'EC'),
+('3', 'EC101', '45', 'EC'),
+('3', 'EC102', '50', 'EC');
+
+select * from students;
+select * from exam;
---
+++ Assignments/Assignment - 4 [Module - DataBase]/SQL Queries/2.sql
@@ -0,0 +1,67 @@
+create database db1;
+
+use db1;
+
+create table Employee (
+Employee_id int auto_increment primary key,
+First_Name varchar(30),
+Last_Name varchar(30),
+Salary int,
+Joining_Date datetime,
+Department varchar(20)
+);
+
+insert into Employee (
+First_Name, 
+Last_Name, 
+Salary, 
+Joining_Date, 
+Department
+)
+values 
+("John", "Abraham", "1000000", "2013-01-01 12:00:00", "Banking"),
+("Michael", "Clarke", "800000", "2013-01-01 12:00:00", "Insurance"),
+("Roy", "Thomas", "700000", "2013-02-01 12:00:00", "Banking"),
+("Tom", "Hose", "600000", "2013-02-01 12:00:00", "Insurance"),
+("Jerry", "Pinto", "650000", "2013-02-01 12:00:00", "Insurance"),
+("Philip", "Mathew", "750000", "2013-01-01 12:00:00", "Services"),
+("TestName1", "123", "650000", "2013-01-01 12:00:00", "Services"),
+("TestName2", "LName%", "600000", "2013-02-01 12:00:00", "Insurance");
+
+select * from employee;
+
+create table Incentives (
+Employee_Ref_id int,
+Incentive_Date datetime,
+Incentive_Amount int,
+foreign key (Employee_Ref_id) references employee (Employee_id)
+);
+
+insert into Incentives (
+Employee_Ref_id,
+Incentive_Date,
+Incentive_Amount
+)
+values
+("1", "2013-02-01", "5000"),
+("2", "2013-02-01", "3000"),
+("3", "2013-02-01", "4000"),
+("1", "2013-01-01", "4500"),
+("2", "2013-01-01", "3500");
+
+select * from Incentives;
+
+SELECT First_Name FROM Employee WHERE First_Name = 'Tom';
+
+select First_Name, Joining_Date, Salary from Employee; 
+
+select * from Employee order by First_Name asc;
+
+select * from Employee order by Salary desc;
+
+SELECT * FROM Employee WHERE first_name LIKE '%J%';
+
+SELECT department, MAX(salary) AS max_salary
+FROM employee
+GROUP BY department
+ORDER BY max_salary ASC;
